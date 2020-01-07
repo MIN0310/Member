f15e6181a24da34f4a19c9dd49658193fd12e57d
miia <miia@com.tw>
2020-01-07 14:09:12 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,7 +2,11 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView edData = findViewById(R.id.data);
+        if(TextUtils.isEmpty(edData.getText().toString())){
+            Intent intent = new Intent(this, NicknameActivity.class);
+            startActivityForResult(intent, MODE_PRIVATE);
+        }
     }
 }
--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -3,13 +3,16 @@ package com.fju.member;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NicknameActivity extends AppCompatActivity {
+
+    private EditText edNickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickname);
-
+        edNickname = findViewById(R.id.nickname);
     }
 }
