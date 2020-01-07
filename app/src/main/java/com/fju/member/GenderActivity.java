172991bc030555d744086955f26327a03659a956
miia <miia@com.tw>
2020-01-07 15:28:23 +0800

    3rd(加Switch)

--- app/src/main/java/com/fju/member/GenderActivity.java
+++ app/src/main/java/com/fju/member/GenderActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class GenderActivity extends AppCompatActivity {
 
     private EditText edGender;
+    boolean isNext = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,17 @@ public class GenderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gender);
         edGender = findViewById(R.id.gender);
 
+        Switch sw = findViewById(R.id.switch2);
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isNext = isChecked;
+                TextView text = findViewById(R.id.switch2);
+                text.setText(isNext?"女" : "男");
+                EditText text2 = findViewById(R.id.gender);
+                text2.setText(isNext?"女" : "男");
+            }
+        });
     }
     public void button(View view){
         String gender = edGender.getText().toString();
